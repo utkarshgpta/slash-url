258511f06fddcb6887fcd1342c378d8b924849e9
Utkarsh Gupta <utkarshg1408@gmail.com>
2017-12-19 04:50:59 +0530

    Ports fixed

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ var config = require('./config.js');
 var baseHash = require('./baseHash.js');
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.set('port', (process.env.PORT || 5000));
+
 var db_url = config.db.host + ':' + config.db.port;
 var rClient = redis.createClient();
 
@@ -120,6 +122,6 @@ app.get('/:encoded_id', function(req, res){
 	});
 });
 
-var server = app.listen(3000, function(){
-  console.log('Server listening on port 3000');
+var server = app.listen(app.get('port'), function(){
+  console.log('Server listening on port ' + app.get('port'));
 });
