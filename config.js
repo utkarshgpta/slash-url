751acfe88776c6d97a518e4ff5cc77dc48548797
Utkarsh Gupta <utkarshg1408@gmail.com>
2017-12-20 14:33:04 +0530

    Resolved merge conflicts

--- config.js
+++ config.js
@@ -2,6 +2,7 @@ var config = {};
 
 config.db = {};
 // the URL shortening host - shortened URLs will be this + baseHash ID
+
 // i.e.: http://localhost:5000/3Ys
 config.webhost = 'http://localhost:5000/';
 
