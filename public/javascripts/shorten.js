1ff15fc8eb37948682bace51619bbda4464fe1cd
Utkarsh Gupta <utkarshg1408@gmail.com>
2018-01-21 18:51:47 +0530

    Added copy to clipboard functionality

--- public/javascripts/shorten.js
+++ public/javascripts/shorten.js
@@ -1,3 +1,9 @@
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();   
+});
+
+$('#clipboardTooltip').tooltip({placement: 'bottom',trigger: 'manual'});
+
 $('#short').submit(function(event){
 	var urlText = $('#urlField').val();
 	var config = {
@@ -8,8 +14,23 @@ $('#short').submit(function(event){
     .then(function (response) {
     	var shortUrl = response.data;
     	$('#shortUrl').text(shortUrl);
+      $('#clipboardTooltip').tooltip('hide')
+      .attr('data-original-title', 'Copy to Clipboard')
+      .tooltip('show');
     })
     .catch(function (error) {
     	console.log(error);
     });
 });
+
+function clipboardCopy() {
+  var copyText = document.getElementById("shortUrl").innerText; 
+  var $temp = $("<input>");
+  $("body").append($temp);
+  $temp.val(copyText).select();
+  document.execCommand("copy");
+  $temp.remove();
+  $('#clipboardTooltip').tooltip('hide')
+      .attr('data-original-title', 'Link Copied!')
+      .tooltip('show');
+}
